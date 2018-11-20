cda9d8169a1016c38d957562845ead0ccbfd61ea
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2018-11-20 21:55:30 +0100

    match card for demo

--- index.js
+++ index.js
@@ -1,39 +1,41 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
 var mongo = require("mongodb").MongoClient;
-const {HLTV} = require("hltv");
+const { HLTV } = require("hltv");
 
 var app = express();
 app.use(express.static('public'));
 
 nunjucks.configure("views", {
-    autoescape: true,
-    express: app
+	autoescape: true,
+	express: app
 
 });
 
-function downloadMatches(collec)
-{
+function downloadMatches(collec) {
 	HLTV.getMatches().then(matches => {
 		collec.insertMany(matches);
 	})
 }
 
-mongo.connect("mongodb://127.0.0.1:27017", {useNewUrlParser: true}, function(error, client) {
+mongo.connect("mongodb://127.0.0.1:27017", { useNewUrlParser: true }, function (error, client) {
 	if (error) return funcCallback(error);
+	console.log("Connected to database");
+
+
 	const db = client.db("counterbet");
 	const matches = db.collection("matches");
 
-	console.log("Connecté à la base de données !");
-
 	//downloadMatches(matches);
 
-	app.get('/', function(req, res) {
+	app.get('/', function (req, res) {
 		matches.find().toArray().then(data => {
-			res.render('index.html', {matches: data});
+			res.render('index.html', { matches: data });
 		});
-	}).get('/teams', function(req, res) {
-		res.render('teams.html', {teams: [{name: "Astralis"}, {name: "Astralis"}]});
+	}).get('/teams', function (req, res) {
+		res.render('teams.html', { teams: [{ name: "Astralis" }, { name: "Astralis" }] });
 	});
 	app.listen(8080);
+
+	console.log("Listening on port 8080 !");
 });
