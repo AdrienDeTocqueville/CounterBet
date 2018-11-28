ed05a51f139ae9fdc1d4efa5d786233b44c8b0cb
Adrien de Tocqueville <adrien.detocqueville@gmail.com>
2018-11-28 12:29:50 +0100

    async and corrected upcoming matches display

--- server.js
+++ server.js
@@ -27,18 +27,17 @@ db.connect().then(() => {
 		console.log(error)
 	});
 
-	app.get('/', function (req, res) {
-		db.getUpcomingMatches().then(matches => {
-			res.render('index.html', { matches , date : (d) => ( date(d)) });
-		});
-	}).get('/teams/:team', function (req, res) {
-		db.getTeam(req.params.team).then(team => {
-			res.render('team.html', team);
-		});
-	}).get('/match/:match', function (req, res) {
-		db.getMatch(req.params.match).then(match => {
-			res.render('match.html', match);
-		});
+	app.get('/', async function (req, res) {
+		let matches = await db.getUpcomingMatches();
+		res.render('index.html', { matches , date : (d) => ( date(d)) });
+	})
+	.get('/teams/:team', async function (req, res) {
+		let team = await db.getTeam(req.params.team);
+		res.render('team.html', {team});
+	})
+	.get('/match/:match', async function (req, res) {
+		let match = await db.getMatch(req.params.match);
+		res.render('match.html', {match});
 	});
 	app.listen(8080);
 
